6489d66dce1dade1bf4c255cad2fb616f9c35d1d
AmitSheoran-557 <amit22082006@gmail.com>
2024-12-04 15:38:58 +0530

    img convert png to webp and manage spacing

---
+++ assets/js/app.js
@@ -0,0 +1,36 @@
+// ===================================== Accordion js================================================
+document.addEventListener("DOMContentLoaded", () => {
+    const headers = document.querySelectorAll(".accordion-header");
+  
+    headers.forEach((header) => {
+      header.addEventListener("click", () => {
+        const content = header.nextElementSibling;  
+        const icon = header.querySelector(".icon");
+  
+        headers.forEach((otherHeader) => {
+          const otherContent = otherHeader.nextElementSibling;
+          const otherIcon = otherHeader.querySelector(".icon");
+  
+          if (otherHeader !== header) {
+            otherContent.classList.add("hidden");
+            otherContent.style.maxHeight = null;
+            otherIcon.classList.remove("active");
+            otherIcon.src = "/assets/image/svg/plus-icon.svg";
+          }
+        });
+  
+        const isHidden = content.classList.contains("hidden");
+        content.classList.toggle("hidden");
+  
+        if (isHidden) {
+          content.style.maxHeight = content.scrollHeight + "px";
+        } else {
+          content.style.maxHeight = null;
+        }
+        icon.classList.toggle("active");
+        icon.src = isHidden
+          ? "assets/image/svg/minus-icon.svg"
+          : "/assets/image/svg/plus-icon.svg";
+      });
+    });
+  });
